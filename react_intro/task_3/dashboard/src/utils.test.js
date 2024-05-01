87cbb2f855ec4665c18889ec3940a7f147b6c96c
carlalap <carlalap>
2024-05-01 16:36:32 -0400

    task 4. Create basic tests with four tests added

---
+++ react_intro/task_3/dashboard/src/utils.test.js
@@ -0,0 +1,20 @@
+import { getFullYear, getFooterCopy, getLatestNotification } from './utils';
+
+// test to check that the function getFullYear returns the correct year
+test('getFullYear returns the current year', () => {
+    expect(getFullYear()).toBe(new Date().getFullYear());
+});
+
+// test to check that getFooterCopy returns the correct string when the argument is true or false
+test('getFooterCopy returns correct string when argument is True', () => {
+    expect(getFooterCopy()).toBe("Atlas School");
+});
+
+test('getFooterCopy returns string when argument is False', () => {
+    expect(getFooterCopy()).toBe("Holberton School");
+});
+
+// test checking the returned string for getLatestNotification
+test('getLatestNotification returns correct string', () => {
+    expect(getLatestNotification()).toBe("<strong>Urgent requirement</strong> - complete by EOD");
+  });
