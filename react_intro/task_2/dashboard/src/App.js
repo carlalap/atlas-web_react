b90d7eb40b7f88c0693ca5b11b01a43f6c885168
carlalap <carlalap>
2024-05-01 16:13:10 -0400

    task3. Modify the Notifications added

--- react_intro/task_2/dashboard/src/App.js
+++ react_intro/task_2/dashboard/src/App.js
@@ -1,13 +1,17 @@
+import React from 'react';
 import logo from './holberton-logo.jpg';
 import './App.css';
 import { getFullYear, getFooterCopy } from './utils';
 
 function App() {
   return (
+    
     <div className="App">
       <header className="App-header">
+      
         <img src={logo} className="App-logo" alt="logo" />
         <h1>School dashboard</h1>
+        
       </header>
 
       <body className="App-body">
