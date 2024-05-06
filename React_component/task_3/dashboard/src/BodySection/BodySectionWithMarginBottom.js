f2968a370223d04bcf268a405bbf6f60af256411
carlalap <carlalap>
2024-05-06 15:28:10 -0400

    BodySection added

---
+++ React_component/task_3/dashboard/src/BodySection/BodySectionWithMarginBottom.js
@@ -0,0 +1,22 @@
+import React from 'react';
+import BodySection from './BodySection'
+import PropTypes from 'prop-types';
+import './BodySectionWithMarginBottom.css'
+
+export default class BodySectionWithMarginBottom extends React.Component {
+  render() {
+    return (
+      <div className='bodySectionWithMargin'>
+        <BodySection title={this.props.title} children={this.props.children}></BodySection>
+      </div>
+    );
+  }
+}
+
+BodySectionWithMarginBottom.propTypes = {
+    title: PropTypes.string
+}
+  
+BodySectionWithMarginBottom.defaultProps = {
+    title: ''
+}
