8d9dc2b0ebccb1f25a7d011a2aea123e540174ad
carlalap <carlalap>
2024-05-27 15:07:28 -0500

    task 3,4 5. mapStateToProps updated, Actions creatos connected & Refactor your code added

---
+++ react_redux_connectors_and_providers/task_1/dashboard/src/App/App.test.js
@@ -0,0 +1,53 @@
+// App.test.js
+import { shallow, mount, configure } from "enzyme";
+import Adapter from '@cfaester/enzyme-adapter-react-18';
+import React from "react";
+import App, { mapStateToProps } from "./App";
+import { StyleSheetTestUtils } from "aphrodite";
+import { fromJS } from "immutable";
+import { configureStore } from '@reduxjs/toolkit';
+import { Provider } from "react-redux";
+import uiReducer, { initialState } from "../reducers/uiReducer";
+
+// Configure Enzyme for React 18
+configure({ adapter: new Adapter() });
+
+// Create Store
+const store = configureStore({
+  reducer: {
+    ui: uiReducer,
+  },
+  preloadedState: {
+    ui: initialState,
+  },
+});
+
+describe("<App />", () => {
+  // Suppress Aphrodite style injection for tests
+  beforeAll(() => {
+    StyleSheetTestUtils.suppressStyleInjection();
+  });
+
+ // Restore Aphrodite style injection after tests
+  afterAll(() => {
+    StyleSheetTestUtils.clearBufferAndResumeStyleInjection();
+  });
+
+  it("renders without crashing", () => {
+    const wrapper = mount(
+      <Provider store={store}>
+        <App />
+      </Provider>
+    );
+    expect(wrapper.exists()).toBe(true);
+  });
+
+  it("mapStateToProps returns the right object from user Login", () => {
+    let state = fromJS({
+      ui: { isUserLoggedIn: true },
+    });
+
+    const result = mapStateToProps(state.toJS());
+    expect(result).toEqual({ isLoggedIn: true });
+  }); 
+});
---
+++ react_redux_connectors_and_providers/task_1/dashboard/src/schema/notifications.js
@@ -0,0 +1,52 @@
+/* notificarions.js file
+* Import the JSON data from notifications.json
+* return: return data from the notifications.json 
+* when the author id is the same as the userId
+*/
+import * as data from '../../notifications.json';
+import { schema, normalize } from 'normalizr';
+
+// creates entities
+const user = new schema.Entity("users")
+const  message = new schema.Entity("messages", {}, {  idAttribute: 'guid' } )
+const notification = new schema.Entity("notifications", {
+  author: user,
+  context: message
+})
+
+// normalize the data
+const normalizedData = normalize(data.default, [notification]);
+
+const getAllNotificationsByUser = (userId) => {
+  // extract normalied entities and result
+  const { entities } = normalizedData;
+
+  // check if notifications are present and result is valid
+  const notificationIds = Object.values(entities.notifications || {});
+  const userNotifications = [];
+
+  // iterate through notificationIds and add valid notifications to userNotifications array
+  for (let i = 0; i < notificationIds.length; i++) {
+    const notification = notificationIds[i];
+    if (entities.users[notification.author].id === userId) {
+      userNotifications.push(notification);
+    }
+  }
+  // extract context from each notification
+  const contextArray = userNotifications.map((notification) => {
+    const messageData = entities.messages[notification.context];
+    return {
+      guid: messageData.guid,
+      isRead: messageData.isRead,
+      type: messageData.type,
+      value: messageData.value
+    };
+  });
+  return contextArray;
+};
+
+export const notificationsNormalizer = (data) => {
+  return normalize(data, [notification]).entities;
+};
+
+export { getAllNotificationsByUser, notification };
